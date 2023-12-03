a3403f873d9dbd7538dcc484655eb2e3896c0b1d
DeadMakar <ssapientiaa@gmail.com>
2023-12-03 15:53:08 +0200

    commit commit

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,17 +1,29 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { ContactListItem } from '../ContactListItem/ContactListItem';
 import {
   StyledContactList,
   StyledContactItem,
   StyledDeleteButton,
 } from './ContactList.styled';
+import { deleteContact } from 'redux/contactsSlice';
 
-export const ContactList = ({ contacts, onDeleteContact }) => {
+export const ContactList = () => {
+  const contacts = useSelector(state => state.contacts.contacts);
+  const filter = useSelector(state => state.filter.filter);
+  const dispatch = useDispatch();
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <StyledContactList>
-      {contacts.map(contact => (
+      {filteredContacts.map(contact => (
         <StyledContactItem key={contact.id}>
           <ContactListItem contact={contact} />
-          <StyledDeleteButton onClick={() => onDeleteContact(contact.id)}>
+          <StyledDeleteButton
+            onClick={() => dispatch(deleteContact(contact.id))}
+          >
             Delete
           </StyledDeleteButton>
         </StyledContactItem>
--- src/components/SearchFilter/SearchFilter.js
+++ src/components/SearchFilter/SearchFilter.js
@@ -1,11 +1,27 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { StyledSearchFilter, ContactTitle } from './SearchFilter.styled';
+import { changeFilter } from 'redux/filterSlice';
 
-export const SearchFilter = ({ value, onChange }) => (
-  <>
-    <ContactTitle>Contacts</ContactTitle>
-    <StyledSearchFilter>
-      <h2>Search contacts</h2>
-      <input type="text" name="filter" value={value} onChange={onChange} />
-    </StyledSearchFilter>
-  </>
-);
+export const SearchFilter = () => {
+  const filter = useSelector(state => state.filter.filter);
+  const dispatch = useDispatch();
+
+  const handleChange = e => {
+    dispatch(changeFilter(e.target.value));
+  };
+
+  return (
+    <>
+      <ContactTitle>Contacts</ContactTitle>
+      <StyledSearchFilter>
+        <h2>Search contacts</h2>
+        <input
+          type="text"
+          name="filter"
+          value={filter}
+          onChange={handleChange}
+        />
+      </StyledSearchFilter>
+    </>
+  );
+};
